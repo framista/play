d8e77932e511ad3453d5b70b3ad29da87a1b1d63
framista <adajab1@gmail.com>
2020-02-13 00:02:02 +0100

    Add chips

--- src/app.js
+++ src/app.js
@@ -1,21 +1,55 @@
 const windows = document.querySelectorAll('.window')
 const fourlineBoard = document.querySelector('.fourline')
+const childrenBoard = fourlineBoard.children
 
 const rowsBoard = 6
 const columnsBoard = 7
+const valueBoard = new Array(6 * 7)
+const opponent = "opponent"
+const you = "you"
+let player = you
 
 windows.forEach(window => {
     window.addEventListener('mouseenter', e => {
-        const position = parseInt(e.target.id.substring(6))
-        const column = position%columnsBoard
-        checkColumn(column)
+        checkColumn(e)
+    })
+    window.addEventListener('click', e => {
+        addChip(e)
     })
 })
 
-function checkColumn(column) {
+fourlineBoard.addEventListener('mouseleave', () => windows.forEach(window => window.classList.remove("column")))
+
+function checkColumn(e) {
+    const position = parseInt(e.target.id.substring(6))
+    const column = position % columnsBoard
     windows.forEach(window => window.classList.remove("column"))
-    const childrenBoard = fourlineBoard.children
-    for (let i = column; i < rowsBoard * columnsBoard; i += columnsBoard){
+    for (let i = column; i < rowsBoard * columnsBoard; i += columnsBoard) {
         childrenBoard.item(i).classList.add("column")
     }
+}
+
+function addChip(e) {
+    element = e.target
+    if (element.classList.contains("circle")) {
+        element = element.parentElement
+    }
+    const position = parseInt(element.id.substring(6))
+    const column = position % columnsBoard
+    let insertIndex = column + columnsBoard * (rowsBoard - 1)
+    while (true) {
+        if (!valueBoard[insertIndex]) {
+            valueBoard[insertIndex] = player
+            childrenBoard.item(insertIndex).firstElementChild.classList.add(`circle--selected${player}`)
+            break
+        }
+        insertIndex -= columnsBoard
+    }
+    isFinished(position)
+    player = player === "you" ? opponent : you
+}
+
+function isFinished(position) {
+ 
+
 }
